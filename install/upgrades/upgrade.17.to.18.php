344520ec60fd08d6d8873615969349f1c1d90a4e
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-01-26 03:05:20 +0000

    Upgrade for global content block permission names
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@2493 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ install/upgrades/upgrade.17.to.18.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "<p>Fixing permission names...";
+
+$query = "UPDATE ".cms_db_prefix()."permissions set permission_name = 'Add Global Content Blocks', permission_text = 'Add Global Content Blocks' WHERE permission_name = 'Add Html Blobs'";
+$db->Execute($query);
+$query = "UPDATE ".cms_db_prefix()."permissions set permission_name = 'Modify Global Content Blocks', permission_text = 'Modify Global Content Blocks' WHERE permission_name = 'Modify Html Blobs'";
+$db->Execute($query);
+$query = "UPDATE ".cms_db_prefix()."permissions set permission_name = 'Delete Global Content Blocks', permission_text = 'Delete Global Content Blocks' WHERE permission_name = 'Delete Html Blobs'";
+$db->Execute($query);
+
+echo "[done]</p>";
+
+echo '<p>Updating schema version... ';
+
+$query = "UPDATE ".cms_db_prefix()."version SET version = 18";
+$db->Execute($query);
+
+echo '[done]</p>';
+
+# vim:ts=4 sw=4 noet
+?>
--- version.php
+++ version.php
@@ -20,7 +20,7 @@
 
 $CMS_VERSION = "0.12";
 $CMS_VERSION_NAME = "Scarborough";
-$CMS_SCHEMA_VERSION = "17";
+$CMS_SCHEMA_VERSION = "18";
 
 # vim:ts=4 sw=4 noet
 ?>
