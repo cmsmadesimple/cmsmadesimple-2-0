9b9965e46de808e11ae3740c153b9857f711ca33
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-02-15 21:54:15 +0000

    Fixed permission check for Add GCBs in GCB list
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@2600 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/listhtmlblobs.php
+++ admin/listhtmlblobs.php
@@ -91,7 +91,7 @@ if (isset($_GET["message"])) {
 	}
 
 #if ($add) {
-if (check_permission($userid, 'Add Html Blobs'))
+if (check_permission($userid, 'Add Global Content Blocks'))
 	{
 ?>
 	<div class="pageoptions">
