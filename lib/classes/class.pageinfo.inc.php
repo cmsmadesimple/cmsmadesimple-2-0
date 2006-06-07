e384719bd63eeacb280826bdb08f770bdaf60053
tsw <tsw@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-06-07 18:33:17 +0000

    added last_edited_by into pageinfo and plugin to show id, username or fullname
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@2877 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/classes/class.pageinfo.inc.php
+++ lib/classes/class.pageinfo.inc.php
@@ -40,6 +40,7 @@ class PageInfo
 	var $content_modified_date;
         var $content_created_date;
 	var $content_last_modified_date;
+	var $content_last_modified_by_id;
 	var $template_id;
 	var $template_encoding;
 	var $template_modified_date;
@@ -61,7 +62,9 @@ class PageInfo
 		$this->content_metadata = '';
 		$this->content_id_hierarchy = '';
 		$this->content_modified_date = -1;
+		$this->content_created_date = -1;
 		$this->content_last_modified_date = -1;
+                $this->content_last_modified_by_id = -1;
 		$this->content_props = array();
 		$this->template_id = -1;
 		$this->template_modified_date = -1;
@@ -100,12 +103,12 @@ class PageInfoOperations
 
 		if (is_numeric($alias) && strpos($alias, '.') === FALSE && strpos($alias, ',') === FALSE) //Fix for postgres
 		{ 
-			$query = "SELECT c.content_id, c.content_name, c.content_alias, c.menu_text, c.titleattribute, c.hierarchy, c.metadata, c.id_hierarchy, c.prop_names, c.create_date AS c_create_date, c.modified_date AS c_date, c.cachable, t.template_id, t.encoding, t.modified_date AS t_date FROM ".cms_db_prefix()."templates t INNER JOIN ".cms_db_prefix()."content c ON c.template_id = t.template_id WHERE (c.content_alias = ? OR c.content_id = ?) AND c.active = 1";
+			$query = "SELECT c.content_id, c.content_name, c.content_alias, c.menu_text, c.titleattribute, c.hierarchy, c.metadata, c.id_hierarchy, c.prop_names, c.create_date AS c_create_date, c.modified_date AS c_date, c.cachable, c.last_modified_by, t.template_id, t.encoding, t.modified_date AS t_date FROM ".cms_db_prefix()."templates t INNER JOIN ".cms_db_prefix()."content c ON c.template_id = t.template_id WHERE (c.content_alias = ? OR c.content_id = ?) AND c.active = 1";
 			$row = &$db->GetRow($query, array($alias, $alias));
 		}
 		else
 		{
-			$query = "SELECT c.content_id, c.content_name, c.content_alias, c.menu_text, c.titleattribute, c.hierarchy, c.metadata, c.id_hierarchy, c.prop_names, c.create_date AS c_create_date, c.modified_date AS c_date, c.cachable, t.template_id, t.encoding, t.modified_date AS t_date FROM ".cms_db_prefix()."templates t INNER JOIN ".cms_db_prefix()."content c ON c.template_id = t.template_id WHERE c.content_alias = ? AND c.active = 1";
+			$query = "SELECT c.content_id, c.content_name, c.content_alias, c.menu_text, c.titleattribute, c.hierarchy, c.metadata, c.id_hierarchy, c.prop_names, c.create_date AS c_create_date, c.modified_date AS c_date, c.cachable, c.last_modified_by,t.template_id, t.encoding, t.modified_date AS t_date FROM ".cms_db_prefix()."templates t INNER JOIN ".cms_db_prefix()."content c ON c.template_id = t.template_id WHERE c.content_alias = ? AND c.active = 1";
 			$row = &$db->GetRow($query, array($alias));
 		}
 
@@ -122,6 +125,7 @@ class PageInfoOperations
 			$onepageinfo->content_metadata = $row['metadata'];
 			$onepageinfo->content_created_date = $db->UnixTimeStamp($row['c_create_date']);
 			$onepageinfo->content_modified_date = $db->UnixTimeStamp($row['c_date']);
+                        $onepageinfo->content_last_modified_by_id = $row['last_modified_by'];
 			$onepageinfo->content_props = explode(',', $row['prop_names']);
 			$onepageinfo->template_id = $row['template_id'];
 			$onepageinfo->template_encoding = $row['encoding'];
---
+++ plugins/function.last_modified_by.php
@@ -0,0 +1,79 @@
+<?php
+#CMS - CMS Made Simple
+#(c)2004 by Ted Kulp (wishy@users.sf.net)
+#This project's homepage is: http://cmsmadesimple.sf.net
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function smarty_cms_function_last_modified_by($params, &$smarty) 
+{
+        global $gCms;
+	$pageinfo = $gCms->variables['pageinfo'];
+
+        $id = "";
+ 
+	if (isset($pageinfo) && $pageinfo->content_last_modified_by_id > -1)
+	{
+		$id = $pageinfo->content_last_modified_by_id;
+	} else {
+                return "";
+        }
+
+	if(empty($params['format']))
+	{
+		$format = "id";
+	}
+	else
+	{
+		$format = $params['format'];
+                $thisuser = UserOperations::LoadUserByID($id);
+	}
+
+
+
+  if($format==="id") {
+     return $id;
+  } else if ($format==="username") {
+     return $thisuser->username;
+  } else if ($format==="fullname") {
+     return $thisuser->firstname ." ". $thisuser->lastname;
+  } else {
+     return "";
+  }
+}
+
+function smarty_cms_help_function_last_modified_by()
+{
+        ?>
+        <h3>What does this do?</h3>
+        <p>Prints last person that edited this page.  If no format is given, it will default to a ID number of user .</p>
+        <h3>How do I use it?</h3>
+        <p>Just insert the tag into your template/page like: <code>{last_modified_by format="fullname"}</code></p>
+        <h3>What parameters does it take?</h3>
+        <ul>
+                <li><em>(optional)</em>format - id, username, fullname</li>
+        </ul>
+        <?php
+}
+
+function smarty_cms_about_function_last_modified_by() {
+	?>
+	<p>Author: Ted Kulp&lt;tedkulp@users.sf.net&gt;</p>
+	<p>Version: 1.0</p>
+	<p>
+	Change History:<br/>
+	</p>
+	<?php
+}
+?>
