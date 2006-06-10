fd5dfe0bb4c33c824da0123f2fd3b923801088a5
tsw <tsw@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-06-10 18:20:45 +0000

    added sitename to default template
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@2905 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/addtemplate.php
+++ admin/addtemplate.php
@@ -29,7 +29,7 @@ $dflt_content='
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
 <head>
-<title>{title}</title>
+<title>{sitename} - {title}</title>
 {metadata}
 {stylesheet}
 </head>
