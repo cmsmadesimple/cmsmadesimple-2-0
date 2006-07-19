b5067c1711dbca586a23f9264b5d3b8e2ed26d03
westis <westis@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-07-19 16:08:50 +0000

    Added: Russ's ammendments to the Anchor tag, for class, title and tabindex
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3096 3d254a34-79dc-0310-9e5f-be208747d8a0

--- plugins/function.anchor.php
+++ plugins/function.anchor.php
@@ -19,41 +19,59 @@
 function smarty_cms_function_anchor($params, &$smarty)
 {
 	global $gCms;
+	#Added by Russ for class, tabindex and title for anchor 2006/07/19
+	$anchorclass="";
+	$anchortitle="";
+	$anchortabindex="";
+	if (isset($params['anchorclass'])) $anchorclass = ' class="'.$params['anchorclass'].'"';
+    if (isset($params['anchortitle'])) $anchortitle = ' title="'.$params['anchortitle'].'"';
+    if (isset($params['anchortabindex'])) $anchortabindex = ' tabindex="'.$params['anchortabindex'].'"';
+	#End of first part added by Russ 2006/07/19
 	
 	if (isset($_SERVER['REQUEST_URI']))
 	{
 		$url = $_SERVER['REQUEST_URI'].'#'.$params['anchor'];
 		if (isset($params['onlyhref']) && ($params['onlyhref'] == '1' || $params['onlyhref'] == 'true'))
+			#Note if you set 'onlyheref' that is what you get - no class or title or tabindex or text
 			echo $url;
 		else
-			echo '<a href="'.$url.'">'.$params['text'].'</a>';
+			#Line replaced by Russ
+			#	echo '<a href="'.$url.'">'.$params['text'].'</a>';
+			#Line replaced with -  by Russ to reflect class and title for anchor 2006/07/19
+			echo '<a href="'.$url.'"'.$anchorclass.$anchortitle.$anchortabindex.'>'.$params['text'].'</a>';
+			#End of second part added by Russ 2006/07/19
 	}
 }
-
+	#Ammended by Russ for class, tabindex and title for anchor 2006/07/19
 function smarty_cms_help_function_anchor() {
 	?>
 	<h3>What does this do?</h3>
 	<p>Makes a proper anchor link.</p>
 	<h3>How do I use it?</h3>
-	<p>Just insert the tag into your template/page like: <code>{anchor anchor='here' text='Scroll Down'}</code></p>
+	<p>Just insert the tag into your template/page like: <code>{anchor anchor='here' text='Scroll Down' anchorclass='myclass' anchortitle='mytitle' anchortabindex='1'}</code></p>
 	<h3>What parameters does it take?</h3>
 	<p>
 	<ul>
 	<li><tt>anchor</tt> - Where we are linking to.  The part after the #.</li>
 	<li><tt>text</tt> - The text to display in the link.</li>
-	<li><em>(optional)</em> <tt>onlyhref</tt> - Only display the href and not the entire link.</li>
+	<li><tt>anchorclass</tt> - The class for the link, if any</li>
+	<li><tt>anchortitle</tt> - The title to display for the link, if any.</li>
+	<li><tt>anchortabindex</tt> - The numeric tabindex for the link, if any.</li>
+	<li><em>(optional)</em> <tt>onlyhref</tt> - Only display the href and not the entire link. No other options will work</li>
 	</ul>
 	</p>
 	<?php
 }
-
+	#Ammended by Russ for class, tabindex and title for anchor 2006/07/19
 function smarty_cms_about_function_anchor() {
 	?>
 	<p>Author: Ted Kulp&lt;tedkulp@users.sf.net&gt;</p>
-	<p>Version: 1.0</p>
+	<p>Version: 1.1</p>
 	<p>
 	Change History:<br/>
-	None
+	<strong>Update to version 1.1 from 1.0</strong> <em>2006/07/19</em><br/>
+	Russ added the means to insert a title, a tabindex and a class for the anchor link.<br/>
+	</hr>
 	</p>
 	<?php
 }
