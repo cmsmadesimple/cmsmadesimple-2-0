e36cf0b78c496f698f733a2d91930ff248677b88
elijahlofgren <elijahlofgren@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-07-23 23:15:13 +0000

    Apply patch: [#824] function.image.php - Default width, height and alt Thanks Walter Wlodarski!
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3120 3d254a34-79dc-0310-9e5f-be208747d8a0

--- plugins/function.image.php
+++ plugins/function.image.php
@@ -21,34 +21,36 @@ function smarty_cms_function_image($params, &$smarty)
 	global $gCms;
 
 	$text = '';
-	$imgstart = "<img src=";
-	$imgend = "/>";
+	$imgstart = '<img src=';
+	$imgend = '/>';
 	if( !empty($params['src'] ) )
 	{
 		$text = $imgstart .= '"'.$gCms->config['image_uploads_url'].'/'.$params['src'].'"';
-        }
-	if( !empty($params['width'] ) )
-	{
-		$text .= ' width="'.$params['width'].'"';
-	}
-	if( !empty($params['height'] ) )
-	{
-		$text .= ' height="'.$params['height'].'"';
-	}
-	if( !empty($params['alt'] ) )
-	{
-		$text .= ' alt="'.$params['alt'].'"';
-	}
-	if( !empty($params['addtext'] ) )
-	{
-	        $text .= ' ' . $params['addtext'];
-	}
-	if( !empty($text) )
-	{
+		$size = getimagesize($gCms->config['image_uploads_path'].'/'.$params['src']);
+		
+				if( !empty($params['width'] ) ) {
+					$text .= ' width="'.$params['width'].'"';
+				} elseif ($size[0] > 0) {
+					$text .= ' width="'.$size[0].'"';
+				}
+				
+				if( !empty($params['height'] ) ) {
+					$text .= ' height="'.$params['height'].'"';
+				} elseif ($size[1] > 0) {
+					$text .= ' height="'.$size[1].'"';
+				}
+				if( !empty($params['alt'] ) )
+				{
+					$text .= ' alt="'.$params['alt'].'"';
+				} else {
+					$text .= ' alt="['.$params['src'].']"';
+				}
+				if( !empty($params['addtext'] ) )
+				{
+						$text .= ' ' . $params['addtext'];
+				}
 		$text .= $imgend;
-	}
-	else
-	{
+	} else {
 		$text = '<!-- empty results from image plugin -->';
 	}
 	return $text;	
@@ -63,15 +65,13 @@ function smarty_cms_help_function_image()
   <h3>How do I use it?</h3>
   <p>Just insert the tag into your template/page like: <code>{image src="something.jpg"}</code></p>
   <h3>What parameters does it take?</h3>
-  <p>
   <ul>
      <li><em>(required)</em>  <tt>src</tt> - Image filename within your images directory.</li>
-     <li><em>(optional)</em>  <tt>width</tt> - Width of the image within the page</li>
-     <li><em>(optional)</em>  <tt>height</tt> - Height of the image within the page</li>
-     <li><em>(optional)</em>  <tt>alt</tt> - Alt text for the image -- needed for xhtml compliance</li>
+     <li><em>(optional)</em>  <tt>width</tt> - Width of the image within the page. Defaults to true size.</li>
+     <li><em>(optional)</em>  <tt>height</tt> - Height of the image within the page. Defaults to true size.</li>
+     <li><em>(optional)</em>  <tt>alt</tt> - Alt text for the image -- needed for xhtml compliance. Defaults to filename.</li>
      <li><em>(optional)</em>  <tt>addtext</tt> - Additional text to put into the tag</li>
   </ul>
-  </p>
   <?php
 }
 
@@ -79,9 +79,10 @@ function smarty_cms_help_function_image()
 function smarty_cms_about_function_image() 
 {
 ?>
-  <p>Author:  Robert Campbell &lt;calguy1000@hotmail.com&gt;</p>
-  <p>Version 1.0</p>
+  <p>Author:  Robert Campbell &lt;calguy1000@hotmail.com&gt;,</p>
+  <p>Version 1.2</p>
   <p>Change History<br/>
+     1.2 - Added default width, height and alt <small>(contributed by Walter Wlodarski)</small><br />
      1.1 - Added alt param and removed the </img><br />
      1.0 - Initial release<br/>
   </p>
