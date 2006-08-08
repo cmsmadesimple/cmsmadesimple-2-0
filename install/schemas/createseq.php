91525a9d06ebe50337270e9f0f02cc6ecfc15f0b
dittmann <dittmann@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-08-08 17:32:32 +0000

    Use more compact syntax
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3259 3d254a34-79dc-0310-9e5f-be208747d8a0

--- install/schemas/createseq.php
+++ install/schemas/createseq.php
@@ -2,133 +2,35 @@
 
 if (isset($CMS_INSTALL_CREATE_TABLES)) {
 
-	echo "<p>Creating additional_users table sequence...";
-    $max = $db->Execute("SELECT max(additional_users_id) as maxid from ".$db_prefix."additional_users");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-    $db->CreateSequence($db_prefix."additional_users_seq", $max);
+    $table_ids = array(
+        'additional_users'          => array('id' => 'additional_users_id'),
+        'admin_bookmarks'           => array('id' => 'bookmark_id'),
+        'admin_recent_pages'        => array('id' => 'id'),
+        'content'                   => array('id' => 'content_id'),
+        'content_props'             => array('id' => 'content_id'),
+        'css'                       => array('id' => 'css_id'),
+        'events'                    => array('id' => 'event_id'),
+        'event_handlers'            => array('id' => 'handler_id', 'seq' => 'event_handler_seq'),
+        'group_perms'               => array('id' => 'group_perm_id'),
+        'groups'                    => array('id' => 'group_id'),
+        'htmlblobs'                 => array('id' => 'htmlblob_id'),
+        'additional_htmlblob_users' => array('id' => 'additional_htmlblob_users_id'),
+        'permissions'               => array('id' => 'permission_id'),
+        'templates'                 => array('id' => 'template_id'),
+        'users'                     => array('id' => 'user_id'),
+        'userplugins'               => array('id' => 'userplugin_id')
+    );
 
-	echo "[done]</p>";
+    foreach ($table_ids as $tablename => $tableinfo)
+    {
+        echo "<p>Creating $tablename table sequence...";
+        $max = $db->Execute('SELECT max(' . $tableinfo['id'] . ') AS maxid FROM '.$db_prefix.$tablename);
+        $max = ($max && $row = $max->FetchRow()) ? $row['maxid']+1 : 1;
+        $tableinfo['seq'] = isset($tableinfo['seq']) ? $tableinfo['seq'] : $tablename . '_seq';
+        $db->CreateSequence($db_prefix.$tableinfo['seq'], $max);
+        echo "[done]</p>";
+    }
 
-	echo "<p>Creating admin_bookmarks table sequence...";
-
-	$max = $db->Execute("SELECT max(bookmark_id) as maxid from ".$db_prefix."admin_bookmarks");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."admin_bookmarks_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating admin_recent_pages table sequence...";
-
-	$max = $db->Execute("SELECT max(id) as maxid from ".$db_prefix."admin_recent_pages");
-	if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."admin_recent_pages_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating content table sequence...";
-
-	$max = $db->Execute("SELECT max(content_id) as maxid from ".$db_prefix."content");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-    $db->CreateSequence($db_prefix."content_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating content_props table sequence...";
-
-	$max = $db->Execute("SELECT max(content_id) as maxid from ".$db_prefix."content_props");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."content_props_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating css table sequence...";
-
-	$max = $db->Execute("SELECT max(css_id) as maxid from ".$db_prefix."css");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."css_seq", $max);
-
-	echo "[done]</p>";
-	
-	echo "<p>Creating events table sequence...";
-
-	$max = $db->Execute("SELECT max(event_id) as maxid from ".$db_prefix."events");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."events_seq", $max);
-
-	echo "[done]</p>";
-	
-	echo "<p>Creating event_handlers table sequence...";
-
-	$max = $db->Execute("SELECT max(handler_id) as maxid from ".$db_prefix."event_handlers");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."event_handler_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating group_perms table sequence...";
-
-	$max = $db->Execute("SELECT max(group_perm_id) as maxid from ".$db_prefix."group_perms");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."group_perms_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating groups table sequence...";
-
-	$max = $db->Execute("SELECT max(group_id) as maxid from ".$db_prefix."groups");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."groups_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating htmlblobs table sequence...";
-
-	$max = $db->Execute("SELECT max(htmlblob_id) as maxid from ".$db_prefix."htmlblobs");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."htmlblobs_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating additional_htmlblob_users table sequence...";
-
-	$max = $db->Execute("SELECT max(additional_htmlblob_users_id) as maxid from".$db_prefix."additional_htmlblob_users");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."additional_htmlblob_users_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating permissions table sequence...";
-
-	$max = $db->Execute("SELECT max(permission_id) as maxid from ".$db_prefix."permissions");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."permissions_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating templates table sequence...";
-
-	$max = $db->Execute("SELECT max(template_id) as maxid from ".$db_prefix."templates");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."templates_seq", $max);
-
-	echo "[done]</p>";
-	
-
-	echo "<p>Creating users table sequence...";
-
-	$max = $db->Execute("SELECT max(user_id) as maxid from ".$db_prefix."users");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."users_seq", $max);
-
-	echo "[done]</p>";
-
-	echo "<p>Creating users table sequence...";
-
-	$max = $db->Execute("SELECT max(userplugin_id) as maxid from ".$db_prefix."userplugins");
-    if ($max && $row=$max->FetchRow()) $max =$row['maxid']+1; else $max=1;
-	$db->CreateSequence($db_prefix."userplugins_seq", $max);
-
-	echo "[done]</p>";
 }
 
 # vim:ts=4 sw=4 noet
