645aa31e2ac5c34a5f805e5a570ce345b5b6b953
dittmann <dittmann@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-08-22 18:05:27 +0000

    BUGFIX: [#760] Fixes in function.sitemap.php. Section headers and separators are shown, but without link, so that website hierarchy isn't altered. Patch by Simon van der Linden (slightly modified) posted here: http://dev.cmsmadesimple.org/tracker/index.php?func=detail&aid=760&group_id=6&atid=103
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3368 3d254a34-79dc-0310-9e5f-be208747d8a0

--- plugins/function.sitemap.php
+++ plugins/function.sitemap.php
@@ -1,61 +1,47 @@
 <?php
-#CMS - CMS Made Simple
-#(c)2004 by Ted Kulp (wishy@users.sf.net)
-#This project's homepage is: http://cmsmadesimple.sf.net
+
+# CMS - CMS Made Simple
 #
-#This program is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#(at your option) any later version.
+# (c)2004 by Ted Kulp (wishy@users.sf.net)
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# This project's homepage is: http://cmsmadesimple.sf.net
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 function smarty_cms_function_sitemap($params, &$smarty) {
 
         global $gCms;
 
-		$contentops =& $gCms->GetContentOperations();
+        $contentops =& $gCms->GetContentOperations();
         $allcontent = $contentops->GetAllContent();
 
-        # defining variables
-        $menu = "";
+        // defining variables
+        $menu = '';
         $last_level = 0;
         $count = 0;
         $in_hr = 0;
-        // Line added by joeli
-        $notshow_hier = '';
 
-        /* LeisureLarry - Begin */
         $add_elements = isset($params["add_elements"]) ? $params["add_elements"] : 0 ;
         $add_element = explode(",", $add_elements);
-        /* LeisureLarry - End */
 
         foreach ($allcontent as $onecontent)
         {
-                // Start added by joeli
-                if ($notshow_hier != '')
-                {
-                        if (strpos($onecontent->mHierarchy, $notshow_hier) !== 0)
-                        {
-                            $notshow_hier = '';
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                }
-                // End added by joeli
-                #Handy little trick to figure out how deep in the tree we are
-                #Remember, content comes to use in order of how it should be displayed in the tree already
+                // Handy little trick to figure out how deep in the tree we are
+                // Remember, content comes to use in order of how it should be displayed in the tree already
                 $depth = count(split('\.', $onecontent->Hierarchy()));
 
-                #If hierarchy starts with the start_element (if it's set), then continue on
+                // If hierarchy starts with the start_element (if it's set), then continue on
                 if (isset($params['start_element']))
                 {
                         if (!(strpos($onecontent->Hierarchy(), $params['start_element']) !== FALSE && strpos($onecontent->Hierarchy(), $params['start_element']) == 0))
@@ -73,58 +59,45 @@ function smarty_cms_function_sitemap($params, &$smarty) {
                                 }
                         }
                 }
-                #Now check to make sure we're not too many levels deep if number_of_levels is set
+                // Now check to make sure we're not too many levels deep if number_of_levels is set
                 if (isset($params['number_of_levels']))
                 {
                         $number_of_levels = $params['number_of_levels'] - 1;
                         $base_level = 1;
 
-                        #Is start_element set?  If so, reset the base_level to it's level
+                        // Is start_element set?  If so, reset the base_level to it's level
                         if (isset($params['start_element']))
                         {
                                 $base_level = count(split('\.', $params['start_element'])) + 1;
                         }
-                        #If this element's level is more than base_level + number_of_levels, then scratch it
+                        // If this element's level is more than base_level + number_of_levels, then scratch it
                         if ($base_level + $number_of_levels < $depth)
                         {
                                 continue;
                         }
                 }
 
-                //Not active?  Toss it.
-                if (!$onecontent->Active())
+                // Not active or separator?  Toss it.
+                if (!$onecontent->Active() || !$onecontent->MenuText())
                 {
-					continue;
+                        continue;
                 }
 
-                //It's a separator or section header?  Toss it.
-                if (!$onecontent->HasUsableLink())
-                {
-					continue;
-                }
-
-                //Not shown in menu?  Toss it.
+                // Not shown in menu?  Toss it.
                 if (!$onecontent->ShowInMenu())
                 {
-                        /* LeisureLarry - parameter show */
-                        //If param showall, display also content not shown in menu.
+                        // If param showall, display also content not shown in menu.
                         if (((isset($params['showall']) && $params['showall'] == 1)) or
                         ($add_elements && in_array($onecontent->Alias(),$add_element)))
                         {
                         }
-                        // Start modified by joeli
-                        else
-						{
-							$notshow_hier = $onecontent->mHierarchy;
-							continue;
-                        }
-                        // End modified by joeli
 
+                        else continue;
                 }
 
                 if ($depth < $last_level)
                 {
-                        for ($i = $depth; $i < $last_level; $i++) $menu .= "</li>\n</ul>\n"; //LeisureLarry
+                        for ($i = $depth; $i < $last_level; $i++) $menu .= "</li>\n</ul>\n";
                         if ($depth > 0)
                         {
                                 $menu .= "</li>\n";
@@ -136,8 +109,6 @@ function smarty_cms_function_sitemap($params, &$smarty) {
                 }
                 if ($depth > $last_level)
                 {
-
-                        /* Leisure Larry - Begin */
                         if ((isset($params['class']) && $params['class'] != '') && ($count == 0))
                         {
                                 $menu .= '<ul class="'.$params['class'].'">'."\n";
@@ -146,16 +117,12 @@ function smarty_cms_function_sitemap($params, &$smarty) {
                         {
                                 $menu .= "\n<ul>\n";
                         }
-                        /* Leisure Larry - End */
-
                 }
 
 				if (! ((isset($params['relative']) && $params['relative']==1) &&
 					(isset($gCms->variables['content_id']) && $onecontent->Id() == $gCms->variables['content_id']) ))
 				// we are not going to show current page if relative it's enabled - we'll show only his childs
 				{
-
-						/* Leisure Larry - Begin */
 						$menu .= "<li>";
 
 						if ((isset($params['delimiter']) && $params['delimiter'] != '') && ($depth > 1))
@@ -167,24 +134,30 @@ function smarty_cms_function_sitemap($params, &$smarty) {
 								}
 						}
 
-						$menu .= "<a href=\"".$onecontent->GetURL()."\"";
-						/* Leisure Larry - End */
-
-						if (isset($gCms->variables['content_id']) && $onecontent->Id() == $gCms->variables['content_id'])
-						{
-								$menu .= " class=\"currentpage\"";
+                		// No link if section header.
+                		if ($onecontent->HasUsableLink())
+                		{
+								$menu .= "<a href=\"".$onecontent->GetURL()."\"";
+								if (isset($gCms->variables['content_id']) && $onecontent->Id() == $gCms->variables['content_id'])
+								{
+										$menu .= " class=\"currentpage\"";
+								}
+								if ($onecontent->GetPropertyValue('target') != '')
+								{
+										$menu .= ' target="'.$onecontent->GetPropertyValue('target').'"';
+								}
+								$menu .= ">".$onecontent->MenuText()."</a>";
 						}
-						if ($onecontent->GetPropertyValue('target') != '')
+						else
 						{
-								$menu .= ' target="'.$onecontent->GetPropertyValue('target').'"';
+								$menu .= $onecontent->MenuText();
 						}
-						$menu .= ">".$onecontent->MenuText()."</a>";
 				}
                 $last_level = $depth;
                 $count++;
         }
 
-        for ($i = 0; $i < $last_level; $i++) $menu .= "</li>\n</ul>"; //LeisureLarry
+        for ($i = 0; $i < $last_level; $i++) $menu .= "</li>\n</ul>";
 
         return $menu;
 
@@ -217,13 +190,14 @@ function smarty_cms_about_function_sitemap()
 {
         ?>
         <p>Author: Marcus Deglos &lt;<a href="mailto:md@zioncore.com">md@zioncore.com</a>&gt;</p>
-        <p>Version: 1.22</p>
+        <p>Version: 1.23</p>
         <p>
         Change History:<br/>
+        1.23 - Section headers and separators are shown, but without link (Simon van der Linden)<br />
         1.22 - Modified to use the new parameters class, delimiter, initial und add_elements (LeisureLarry)<br />
         1.21 - Changed help to show the existing parameters relative and showall (LeisureLarry)<br />
-        1.2 - Modified to support alias instead of hierarchy and minor output improvement (intersol).<br />
-        1.1 - Modified to use new content rewrite (wishy)
+        1.2 -  Modified to support alias instead of hierarchy and minor output improvement (intersol).<br />
+        1.1 -  Modified to use new content rewrite (wishy)
         </p>
         <?php
 }
