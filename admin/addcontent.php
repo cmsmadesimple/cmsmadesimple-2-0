11946311d64cc1fa470ae78b479f61580f5782f8
tsw <tsw@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-08-25 17:02:49 +0000

    slight change in previous addcontent fix, no need for editcontent changing it didnt have this problem
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3395 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/addcontent.php
+++ admin/addcontent.php
@@ -475,8 +475,10 @@ $tabnames = $contentobj->TabNames();
 <?php
 
 }
-echo '<p class="pageback"><a class="pageback" href="'.$themeObject->BackUrl().'">&#171; '.lang('back').'</a></p>';
 echo '</div>';
+
+echo '<p class="pageback"><a class="pageback" href="'.$themeObject->BackUrl().'">&#171; '.lang('back').'</a></p>';
+
 include_once("footer.php");
 
 # vim:ts=4 sw=4 noet
