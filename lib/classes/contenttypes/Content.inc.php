5d919e91d4950b272ac96ca3456d17fa9071e4f0
dittmann <dittmann@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-10-09 13:14:21 +0000

    [#1066] Rights for Editor insuffied - Cannot set Content Position. Allow setting the content parent when adding new content and user has 'Add Pages' permissions.\nDeleted a redundant global $gCms statement in the same function EditAsArray.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3463 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/classes/contenttypes/Content.inc.php
+++ lib/classes/contenttypes/Content.inc.php
@@ -197,9 +197,8 @@ class Content extends ContentBase
 	{
 	    $ret[]= array(lang('title').':','<input type="text" name="title" value="'.cms_htmlentities($this->mName).'" />');
 	    $ret[]= array(lang('menutext').':','<input type="text" name="menutext" value="'.cms_htmlentities($this->mMenuText).'" />');
-	    if (check_permission(get_userid(), 'Modify Page Structure'))
+	    if (check_permission(get_userid(), 'Modify Page Structure') || ($adding == true && check_permission(get_userid(), 'Add Pages')))
         {
-			global $gCms;
 			$contentops =& $gCms->GetContentOperations();
             $ret[]= array(lang('parent').':',$contentops->CreateHierarchyDropdown($this->mId, $this->mParentId));
         }
