7e54a065ba0f73b7ef888bd1ead23efcd0353593
tsw <tsw@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-11-22 15:36:38 +0000

    fixed typo in cms selflink, thanks kode_fi
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3502 3d254a34-79dc-0310-9e5f-be208747d8a0

--- plugins/function.cms_selflink.php
+++ plugins/function.cms_selflink.php
@@ -42,7 +42,7 @@ $title=' title="'.$params['title'].'" ';
 
 $target = '';
 if ( isset($params['target']) && ( strlen($params['target'] > 0 ) ) ) {
-$target=' target="'.$params['taget'].'" ';
+$target=' target="'.$params['target'].'" ';
 }
 
 $external_text = '(external link)';
