43cc6585350482b3c49f7a932c150f8cb243dd40
dittmann <dittmann@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-11-28 09:54:33 +0000

    [#1156] Unused getURL function definition in page.functions.php - patch submitted by wolog here: http://dev.cmsmadesimple.org/tracker/index.php?func=detail&aid=1156&group_id=6&atid=103
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3528 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/page.functions.php
+++ lib/page.functions.php
@@ -941,43 +941,6 @@ function check_access($page_id)
  }
 
 
-/**
- * Takes a page object as input
- * Returns the page's URL as a string.
- * If the page cannot have a valid URL (e.g. content_type='seperator'), it returns an empty string.
- */
-function getURL($page)
-{
-	global $config;
-	$url = "";
-
-	# Fix URL where appropriate
-	if ($page->page_type == "link")
-	{
-		$url = $page->page_url;
-	}
-	elseif ($page->page_type != "sectionheader")
-	{
-		if (isset($page->page_alias) && $page->page_alias != "")
-		{
-			if ($config["assume_mod_rewrite"])
-				$url = $config["root_url"]."/".$page->page_alias.$config["page_extension"];
-			else
-				$url = $config["root_url"]."/index.php?".$config["query_var"]."=".$page->page_alias;
-
-		}
-		else
-		{
-			if ($config["assume_mod_rewrite"])
-				$url = $config["root_url"]."/".$page->page_id.$config["page_extension"];
-			else
-				$url = $config["root_url"]."/index.php?".$config["query_var"]."=".$page->page_id;
-		}
-	}
-	return $url;
-
- }
-
 function wysiwyg_form_submit()
 {
 	global $gCms;
