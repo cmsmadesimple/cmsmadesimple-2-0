0da6c013a8961e70cd266364a435d611b018c756
elijahlofgren <elijahlofgren@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-12-27 21:03:04 +0000

    Display a warning in admin/index.php if CMSMS needs a DB upgrade. (not using lang strings, but upgrade.php doesn't either). Requested here: http://forum.cmsmadesimple.org/index.php/topic,8711.0.html
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3644 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/index.php
+++ admin/index.php
@@ -28,9 +28,25 @@ require_once("../include.php");
 
 check_login();
 
+global $gCms;
+$db =& $gCms->GetDb();
+
 include_once("header.php");
 $themeObject->DisplayDashboardCallout(dirname(dirname(__FILE__)).'/install');
 $themeObject->DisplayDashboardCallout(TMP_CACHE_LOCATION . '/SITEDOWN', lang('sitedownwarning', TMP_CACHE_LOCATION . '/SITEDOWN'));
+
+// Display a warning if CMSMS needs upgrading
+$current_version = $CMS_SCHEMA_VERSION;
+$query = "SELECT version from ".cms_db_prefix()."version";
+$row = $db->GetRow($query);
+if ($row)
+{
+	$current_version = $row["version"];
+}
+if ($current_version < $CMS_SCHEMA_VERSION)
+{
+	echo '<div class="pageerrorcontainer"><div class="pageoverflow"><p class="pageerror"><em><strong>Warning:</strong></em> CMSMS is in need of an upgrade.</p><p>You are now running schema version '.$current_version." and you need to be upgraded to version ".$CMS_SCHEMA_VERSION.'.</p><p>Please click the following link: <a href="'.$config['root_url'].'/install/upgrade.php">Start upgrade process</a>.</p></div></div>';
+}
 $themeObject->ShowShortcuts();
 $themeObject->DisplaySectionMenuDivStart();
 $themeObject->DisplayAllSectionPages();
