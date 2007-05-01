1261fa00f47dff619e896c439c07a88d891bcc3f
silmarillion <silmarillion@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-05-01 19:03:01 +0000

    Added syntax-hooks when adding css/templates
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@3899 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/addtemplate.php
+++ admin/addtemplate.php
@@ -227,12 +227,13 @@ else
 		</div>
 		<div class="pageoverflow">
 			<p class="pagetext">*<?php echo lang('content')?>:</p>
-			<p class="pageinput"><?php echo create_textarea(false, $content, 'content', 'pagebigtextarea', '', $encoding)?></p>
+			
+			<p class="pageinput"><?php echo create_textarea(false, $content, 'content', 'pagebigtextarea', 'content', $encoding, '', '80', '15','','html')?></p>
 		</div>
 		<?php if ($templateops->StylesheetsUsed() > 0) { ?>
 		<div class="pageoverflow">
 			<p class="pagetext"><?php echo lang('stylesheet')?>:</p>
-			<p class="pageinput"><?php echo create_textarea(false, $stylesheet, 'stylesheet', 'pagebigtextarea', '', $encoding)?></p>
+			<p class="pageinput"><?php echo create_textarea(false, $stylesheet, 'stylesheet', 'pagebigtextarea', '', $encoding, '', '80', '15','','css')?></p>
 		</div>
 		<?php } ?>
 		<div class="pageoverflow">
