fe340999418874f8fb1a987f8ad27bf2c756add6
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-08-12 19:28:27 +0000

    Added missing variables for the adsense plugin
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@4074 3d254a34-79dc-0310-9e5f-be208747d8a0

--- plugins/function.adsense.php
+++ plugins/function.adsense.php
@@ -1,7 +1,7 @@
 <?php
 #CMS - CMS Made Simple
-#(c)2004 by Ted Kulp (wishy@users.sf.net)
-#This project's homepage is: http://cmsmadesimple.sf.net
+#(c)2004-2007 by Ted Kulp (ted@cmsmadesimple.org)
+#This project's homepage is: http://cmsmadesimple.org
 #
 #This program is free software; you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
@@ -22,7 +22,13 @@ function smarty_cms_function_adsense($params, &$smarty) {
 	$ad_height = "600";
 	$ad_format = "120x600_as";
 	$ad_channel = "";
-
+	$ad_type = "";
+	$color_border = "";
+	$color_bg = "";
+	$color_link = "";
+	$color_url = "";
+	$color_text = "";
+	
 	if(!empty($params['ad_client']))
 		$ad_client = $params['ad_client'];
 	if(!empty($params['ad_width']))
@@ -33,17 +39,36 @@ function smarty_cms_function_adsense($params, &$smarty) {
 		$ad_format = $params['ad_format'];
 	if(!empty($params['ad_channel']))
 		$ad_channel = $params['ad_channel'];
+	if(!empty($params['ad_type']))
+		$ad_type = $params['ad_type'];
+	if(!empty($params['color_border']))
+		$color_border = $params['color_border'];
+	if(!empty($params['color_bg']))
+		$color_bg = $params['color_bg'];
+	if(!empty($params['color_link']))
+		$color_link = $params['color_link'];
+	if(!empty($params['color_url']))
+		$color_url = $params['color_url'];
+	if(!empty($params['color_text']))
+		$color_text = $params['color_text'];
+
 	
-	$result = "\n<!-- Begin Google AdSense Ad -->\n";
+	//$result = "\n<!-- Begin Google AdSense Ad -->\n";
 	$result .= "\n<script type=\"text/javascript\"><!--\n";
 	$result .= "google_ad_client = \"$ad_client\";\n";
 	$result .= "google_ad_width = \"$ad_width\";\n";
 	$result .= "google_ad_height = \"$ad_height\";\n";
 	$result .= "google_ad_format = \"$ad_format\";\n";
+	$result .= "google_ad_type = \"$ad_type\";\n";
 	$result .= "google_ad_channel = \"$ad_channel\";\n";
+	$result .= "google_color_border = \"$color_border\";\n";
+	$result .= "google_color_bg = \"$color_bg\";\n";
+	$result .= "google_color_link = \"$color_link\";\n";
+	$result .= "google_color_url = \"$color_url\";\n";
+	$result .= "google_color_text = \"$color_text\";\n";
 	$result .= "//--></script>\n";
 	$result .= "<script type=\"text/javascript\" src=\"http://pagead2.googlesyndication.com/pagead/show_ads.js\"></script>\n";
-	$result .= "\n<!-- End Google AdSense Ad -->\n";
+	//$result .= "\n<!-- End Google AdSense Ad -->\n";
 
 	return $result;
 }
@@ -62,6 +87,11 @@ function smarty_cms_help_function_adsense() {
 		<li>ad_height - height of the ad</li>
 		<li>ad_format - "format" of the ad <em>e.g. 120x600_as</em></li>
 		<li>ad_channel - channels are an advanced feature of adsense.  Put it here if you use it.</li>
+		<li>ad_type - possible options are text, image or text_image.</li>
+		<li>color_border - the color of the border. Use HEX color or type the color name (Ex. Red)</li>
+		<li>color_link - the color of the linktext. Use HEX color or type the color name (Ex. Red)</li>
+		<li>color_url - the color of the URL. Use HEX color or type the color name (Ex. Red)</li>
+		<li>color_text - the color of the text. Use HEX color or type the color name (Ex. Red)</li>
 	</ul>
 	</p>
 	<?php
@@ -73,7 +103,7 @@ function smarty_cms_about_function_adsense() {
 	<p>Version: 1.0</p>
 	<p>
 	Change History:<br/>
-	None
+	11/11/2005 Hacked by basicus (http://basicus.com) Added the missing configuration options that AdSense provides.
 	</p>
 	<?php
 }
