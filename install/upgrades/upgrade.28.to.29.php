0655c1971f3022324195655a916738770b3b772d
tsw <tsw@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-09-29 17:46:27 +0000

    Install printing on upgrade
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.2.x@4201 3d254a34-79dc-0310-9e5f-be208747d8a0

--- install/upgrades/upgrade.28.to.29.php
+++ install/upgrades/upgrade.28.to.29.php
@@ -1,6 +1,6 @@
 <?php
 
-echo '<p>Installing new FileManager... ';
+echo '<p>Installing new FileManager module... ';
 
 $modops =& $gCms->GetModuleOperations();
 $result = $modops->InstallModule('FileManager',false);
@@ -14,8 +14,22 @@ else
     echo '<p class="okmessage">'.$gCms->modules['FileManager']['object']->InstallPostMessage().'</p>';
 }
 
+echo '<p>Installing new Printing module... ';
+
+$modops =& $gCms->GetModuleOperations();
+$result = $modops->InstallModule('Printing',false);
+if( $result[0] == false )
+{
+    echo '[failed]</p>';
+}
+else
+{
+    echo '[done]</p>';
+    echo '<p class="okmessage">'.$gCms->modules['Printing']['object']->InstallPostMessage().'</p>';
+}
+
 echo '<p>Updating schema version... ';
-$query = 'UPDATE '.cms_db_prefix().'version SET version = 29';
+#$query = 'UPDATE '.cms_db_prefix().'version SET version = 29';
 $db->Execute($query);
 echo '[done]</p>';  
 
