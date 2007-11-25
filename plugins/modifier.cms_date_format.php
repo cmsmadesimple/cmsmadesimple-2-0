fdc52013af422a873d43dd5569001a42a13b07c1
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-11-25 16:18:24 +0000

    Fixes to cms_date_format.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.2.x@4263 3d254a34-79dc-0310-9e5f-be208747d8a0

--- plugins/modifier.cms_date_format.php
+++ plugins/modifier.cms_date_format.php
@@ -1,9 +1,16 @@
 <?php
 
+global $gCms;
+$smarty =& $gCms->GetSmarty();
+$config =& $gCms->GetConfig();
+$fn = cms_join_path($config['root_path'],'lib','smarty','plugins','modifier.date_format.php');
+if( !file_exists($fn) ) die();
+require_once( $fn );
 
 function smarty_cms_modifier_cms_date_format($string, $format = '',
 					     $default_date = '')
 {
+
   if( $format == '' )
     {
       $format = get_site_preference('defaultdateformat');
@@ -12,7 +19,7 @@ function smarty_cms_modifier_cms_date_format($string, $format = '',
 	  $format = '%b %e, %Y';
 	}
       $uid = get_userid(false);
-      if( isset($CMS_ADMIN_PAGE) && $uid )
+      if( $uid )
 	{
 	  $tmp = get_preference($uid,'date_format_string');
 	  if( $tmp != '' )
