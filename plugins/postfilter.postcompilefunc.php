82618e1555d273d4933d11cafa88cdae5b7e1b03
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2008-02-04 19:53:07 +0000

    Removed the old style module callbacks from the plugins directory
    Signed-off-by: Ted Kulp <ted@cmsmadesimple.org>
    
    
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@4334 3d254a34-79dc-0310-9e5f-be208747d8a0

--- plugins/postfilter.postcompilefunc.php
+++ plugins/postfilter.postcompilefunc.php
@@ -25,63 +25,25 @@ function smarty_cms_postfilter_postcompilefunc($tpl_output, &$smarty)
 	{
 		switch ($result[0])
 		{
-			case "content":
-				#Perform the content prerender callback
-				foreach($gCms->modules as $key=>$value)
-				{
-					if ($gCms->modules[$key]['installed'] == true &&
-						$gCms->modules[$key]['active'] == true)
-					{
-						$gCms->modules[$key]['object']->ContentPostCompile($tpl_output);
-					}
-				}
-				
-				CmsEvents::SendEvent('Core', 'ContentPostCompile', array('content' => &$tpl_output));
-				
+			case "content":				
+				CmsEvents::send_event('Core', 'ContentPostCompile', array('content' => &$tpl_output));
 				break;
+
 			case "template":
-				#Perform the content prerender callback
-				foreach($gCms->modules as $key=>$value)
-				{
-					if ($gCms->modules[$key]['installed'] == true &&
-						$gCms->modules[$key]['active'] == true)
-					{
-						$gCms->modules[$key]['object']->TemplatePostCompile($tpl_output);
-					}
-				}
-				
-				CmsEvents::SendEvent('Core', 'TemplatePostCompile', array('template' => &$tpl_output));
-				
+				CmsEvents::send_event('Core', 'TemplatePostCompile', array('template' => &$tpl_output));
 				break;
-			case "globalcontent":
-				foreach($gCms->modules as $key=>$value)
-				{
-					if ($gCms->modules[$key]['installed'] == true &&
-						$gCms->modules[$key]['active'] == true)
-					{
-						$gCms->modules[$key]['object']->GlobalContentPostCompile($tpl_output);
-					}
-				}
-				
-				CmsEvents::SendEvent('Core', 'GlobalContentPostCompile', array('global_content' => &$tpl_output));
-				
+
+			case "globalcontent":				
+				CmsEvents::send_event('Core', 'GlobalContentPostCompile', array('global_content' => &$tpl_output));
 				break;
+
 			default:
 				break;
 		}
 
 	}
-
-	foreach($gCms->modules as $key=>$value)
-	{
-		if ($gCms->modules[$key]['installed'] == true &&
-			$gCms->modules[$key]['active'] == true)
-		{
-			$gCms->modules[$key]['object']->SmartyPostCompile($tpl_output);
-		}
-	}
 	
-	CmsEvents::SendEvent('Core', 'SmartyPostCompile', array('content' => &$tpl_output));
+	CmsEvents::send_event('Core', 'SmartyPostCompile', array('content' => &$tpl_output));
 
 	return $tpl_output;
 }
--- plugins/prefilter.precompilefunc.php
+++ plugins/prefilter.precompilefunc.php
@@ -26,60 +26,24 @@ function smarty_cms_prefilter_precompilefunc($tpl_output, &$smarty)
 		switch ($result[0])
 		{
 			case "content":
-				foreach($gCms->modules as $key=>$value)
-				{
-					if ($gCms->modules[$key]['installed'] == true &&
-						$gCms->modules[$key]['active'] == true)
-					{
-						$gCms->modules[$key]['object']->ContentPreCompile($tpl_output);
-					}
-				}
-				
-				CmsEvents::SendEvent('Core', 'ContentPreCompile', array('content' => &$tpl_output));
-				
+				CmsEvents::send_event('Core', 'ContentPreCompile', array('content' => &$tpl_output));				
 				break;
-			case "template":
-				foreach($gCms->modules as $key=>$value)
-				{
-					if ($gCms->modules[$key]['installed'] == true &&
-						$gCms->modules[$key]['active'] == true)
-					{
-						$gCms->modules[$key]['object']->TemplatePreCompile($tpl_output);
-					}
-				}
-				
-				CmsEvents::SendEvent('Core', 'TemplatePreCompile', array('template' => &$tpl_output));
-				
+
+			case "template":				
+				CmsEvents::send_event('Core', 'TemplatePreCompile', array('template' => &$tpl_output));
 				break;
-			case "globalcontent":
-				foreach($gCms->modules as $key=>$value)
-				{
-					if ($gCms->modules[$key]['installed'] == true &&
-						$gCms->modules[$key]['active'] == true)
-					{
-						$gCms->modules[$key]['object']->GlobalContentPreCompile($tpl_output);
-					}
-				}
-				
-				CmsEvents::SendEvent('Core', 'GlobalContentPreCompile', array('global_content' => &$tpl_output));
-				
+
+			case "globalcontent":				
+				CmsEvents::send_event('Core', 'GlobalContentPreCompile', array('global_content' => &$tpl_output));
 				break;
+
 			default:
 				break;
 		}
 
 	}
-
-	foreach($gCms->modules as $key=>$value)
-	{
-		if ($gCms->modules[$key]['installed'] == true &&
-			$gCms->modules[$key]['active'] == true)
-		{
-			$gCms->modules[$key]['object']->SmartyPreCompile($tpl_output);
-		}
-	}
 	
-	CmsEvents::SendEvent('Core', 'SmartyPreCompile', array('content' => &$tpl_output));
+	CmsEvents::send_event('Core', 'SmartyPreCompile', array('content' => &$tpl_output));
 
 	return $tpl_output;
 }
