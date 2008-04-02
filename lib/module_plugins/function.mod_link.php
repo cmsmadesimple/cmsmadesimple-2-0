9ee6a9d23fc48d62e001c78876b2675cc3e466eb
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2008-04-02 01:34:22 +0000

    Tweaks to mod_link
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@4398 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/module_plugins/function.mod_link.php
+++ lib/module_plugins/function.mod_link.php
@@ -21,21 +21,30 @@ function smarty_cms_function_mod_link($params, &$smarty)
 	$module =& $smarty->get_template_vars('cms_mapi_module');
 	$id = $smarty->get_template_vars('cms_mapi_id');
 	$return_id = $smarty->get_template_vars('cms_mapi_return_id');
+	$translate = coalesce_key($params,'translate',true);
 
-	$value = (isset($params['translate']) && $params['translate'] == true) ? $module->lang($params['value']) : $params['value'];
+	$value = ($translate === true) ? $module->lang($params['value']) : $params['value'];
 	if (isset($params['theme_image']))
 	{
 		$themeObject = CmsAdminTheme::get_instance();
-		$value = $themeObject->display_image($params['theme_image'], $value,'','','systemicon');
+		$image = $themeObject->display_image($params['theme_image'], $value,'','','systemicon');
+		if( isset($params['showtext']) )
+		  {
+		    $value = $image.'&nbsp;'.$value;
+		  }
+		else
+		  {
+		    $value = $image;
+		  }
 	}
 
 	$other_params = remove_keys($params, array('action', 'value', 'warn_message', 'translate', 'only_href', 
 						'inline', 'additional_text', 'target_container_only', 'pretty_url', 'theme_image'));
 
 	$blah = $module->create_link($id, $params['action'], $return_id, $value, 
-						$other_params, coalesce_key($params, 'warn_message', ''), coalesce_key($params, 'only_href', false), 
-						coalesce_key($params, 'inline', false), coalesce_key($params, 'additional_text', ''), 
-						coalesce_key($params, 'target_container_only', false), coalesce_key($params, 'pretty_url', ''));
+				     $other_params, coalesce_key($params, 'warn_message', ''), coalesce_key($params, 'only_href', false), 
+				     coalesce_key($params, 'inline', false), coalesce_key($params, 'additional_text', ''), 
+				     coalesce_key($params, 'target_container_only', false), coalesce_key($params, 'pretty_url', ''));
 						
 	return $blah;
 }
