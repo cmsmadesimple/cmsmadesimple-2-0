593065de69ef2a1ae107bb78a2c56b41329fc667
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2008-04-26 15:07:29 +0000

    Adds two more module plugins (mod_helptext, and mod_formrow)
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@4449 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ lib/module_plugins/function.mod_helptext.php
@@ -0,0 +1,32 @@
+<?php
+#CMS - CMS Made Simple
+#(c)2004-2006 by Ted Kulp (ted@cmsmadesimple.org)
+#This project's homepage is: http://cmsmadesimple.org
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function smarty_cms_function_mod_helptext($params, &$smarty)
+{
+	static $counter = 1;
+	$module =& $smarty->get_template_vars('cms_mapi_module');
+	$id = $smarty->get_template_vars('cms_mapi_id');
+	$translate = coalesce_key($params, 'translate', true, FILTER_VALIDATE_BOOLEAN);
+	$value = ($translate === true) ? $module->lang($params['value']) : $params['value'];
+	$htmlid = $id.'help_'.$params['name'];
+
+	return $module->create_input_hidden($id, 'help_'.$params['name'], $value, 
+					    coalesce_key($params, 'addttext', ''), $htmlid );
+}
+
+?>
