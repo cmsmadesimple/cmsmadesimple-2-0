d8583cbbb9b941395a69808da9c93f1523adbb0c
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2008-05-20 17:12:27 +0000

    Fix some style name hardcoding.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.2.x@4502 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/loginstyle.php
+++ admin/loginstyle.php
@@ -1,7 +1,12 @@
 <?php 
+$theme=get_site_preference('logintheme', 'default');
 header("Content-type: text/css");
-if (file_exists(dirname(__FILE__)."/themes/default/css/style.css"))
-	{
-	readfile(dirname(__FILE__)."/themes/default/css/style.css");
-}
+if (file_exists(dirname(__FILE__)."/themes/$theme/css/style.css"))
+  {
+    readfile(dirname(__FILE__)."/themes/$theme/css/style.css");
+  }
+else
+  {
+    readfile(dirname(__FILE__)."/themes/default/css/style.css");
+  }
 ?>
