1192ef81c72d0f18723ba846f1b73973aedbd522
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2008-05-20 17:16:18 +0000

    More tweaks to loginstyle.php
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.2.x@4503 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/loginstyle.php
+++ admin/loginstyle.php
@@ -1,4 +1,28 @@
 <?php 
+#CMS - CMS Made Simple
+#(c)2004 by Ted Kulp (wishy@users.sf.net)
+#This project's homepage is: http://cmsmadesimple.sf.net
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#$Id: login.php 4251 2007-11-15 21:34:40Z calguy1000 $
+
+$CMS_ADMIN_PAGE=1;
+
+require_once("../include.php");
+require_once("../lib/classes/class.user.inc.php");
+
 $theme=get_site_preference('logintheme', 'default');
 header("Content-type: text/css");
 if (file_exists(dirname(__FILE__)."/themes/$theme/css/style.css"))
