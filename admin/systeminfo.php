4e4269073039889dd4db53c3228e791d4c7b96d9
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2008-06-14 14:40:57 +0000

    More work on the systeminfo stuff (smartyfying it).
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.2.x@4620 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/systeminfo.php
+++ admin/systeminfo.php
@@ -25,9 +25,28 @@ include_once("header.php");
 
 check_login();
 
+function systeminfo_lang($params,&$smarty)
+{
+  if( count($params) )
+    {
+      $tmp = array();
+      foreach( $params as $k=>$v)
+	{
+	  $tmp[] = $v;
+	}
+      
+      
+      $str = $tmp[0];
+      $tmp2 = array();
+      for( $i = 1; $i < count($tmp); $i++ )
+	$tmp2[] = $params[$i];
+      return lang($str,$tmp2);
+    }
+}
+
 global $gCms;
 $smarty =& $gCms->GetSmarty();
-echo $smarty->fetch('systeminfo.tpl'); exit;
+$smarty->register_function('si_lang','systeminfo_lang');
 
 function nrow($prompt, $text = '',$color = '')
 {
@@ -81,45 +100,38 @@ $config = &$gCms->config;
 echo '<div class="pagecontainer">';
 echo $themeObject->ShowHeader('systeminfo');
 echo '<div class="pageoverflow">';
-echo '<p>'.lang('help_systeminformation')."</p><hr/></div>\n";
 
-echo "<fieldset>\n";
-echo "<legend><strong>".lang('cms_install_information')."</strong>: </legend>\n";
-nrow(lang('cms_version'),$GLOBALS['CMS_VERSION']);
-echo "<h4>".lang('installed_modules')."</h4>\n";
+$smarty->assign('cms_version',$GLOBALS['CMS_VERSION']);
 $query = "SELECT * FROM ".cms_db_prefix()."modules WHERE active=1";
 $modules = $db->GetArray($query);
-ntable($modules,array('module_name'=>lang('module_name'),
-		      'version'=>lang('version')));
-echo "</fieldset><br/>\n";
+$smarty->assign('installed_modules',$modules);
 
-echo "<fieldset>\n";
-echo "<legend><strong>".lang('php_information')."</strong>: </legend>\n";
+$tmp = array();
 $safe_mode = ini_get_boolean('safe_mode');
-nrow(lang('php_safe_mode'),($safe_mode?lang('on'):lang('off')),
-	  ($safe_mode)?'red':'');
-nrow(lang('current_php_version'),phpversion());
-nrow(lang('maxiumum_post_size'),get_cfg_var('post_max_size'));
-nrow(lang('maxiumum_upload_size'),get_cfg_var('upload_max_filesize'));
-nrow(lang('php_memory_limit'),get_cfg_var('memory_limit'));
-nrow(lang('php_max_execution_time'),get_cfg_var('max_execution_time'));
-$tmp = session_save_path();
-nrow(lang('session_save_path'),$tmp,empty($tmp)?'red':'');
-echo "</fieldset><br/>\n";
+$tmp['safe_mode'] = array(($safe_mode?lang('on'):lang('off')), ($safe_mode)?'red':'');
+$tmp['current_php_version'] = phpversion();
+$tmp['php_memory_limit'] = get_cfg_var('memory_limit');
+$tmp['maximum_post_size'] = get_cfg_var('post_max_size');
+$tmp['maximum_upload_size'] = get_cfg_var('upload_max_filesize');
+$tmp['php_max_execution_time'] = get_cfg_var('max_execution_time');
+$tmp2 = session_save_path();
+$tmp['session_save_path'] = array($tmp2, empty($tmp2)?'red':'');
+$smarty->assign('php_information',$tmp);
 
-// Note: we should be displaying some funky stuff for postgres
-// Here too I guess.
-echo "<fieldset>\n";
-echo "<legend><strong>".lang('mysql_information')."</strong>: </legend>\n";
-nrow(lang('mysql_server_version'),mysql_get_server_info());
-echo "</fieldset><br/>\n";
 
-echo "<fieldset>\n";
-echo "<legend><strong>".lang('server_information')."</strong>: </legend>\n";
-nrow(lang('server_software'),$_SERVER['SERVER_SOFTWARE']);
-nrow(lang('server_api'),PHP_SAPI);
-nrow(lang('server_os'),PHP_OS.' v '.php_uname('r').' '.lang('on').' '.php_uname('m'));
-echo "</fieldset><br/>\n";
+// // Note: we should be displaying some funky stuff for postgres
+// // Here too I guess.
+// echo "<fieldset>\n";
+// echo "<legend><strong>".lang('mysql_information')."</strong>: </legend>\n";
+// nrow(lang('mysql_server_version'),mysql_get_server_info());
+// echo "</fieldset><br/>\n";
+
+$tmp = array();
+$tmp['server_software'] = $_SERVER['SERVER_SOFTWARE'];
+$tmp['server_api'] = PHP_SAPI;
+$tmp['server_os'] = PHP_OS.' v '.php_uname('r').' '.lang('on').' '.php_uname('m');
+$smarty->assign('server_info',$tmp);
+echo $smarty->fetch('systeminfo.tpl'); exit;
 
 clearstatcache();
 
--- admin/templates/systeminfo.tpl
+++ admin/templates/systeminfo.tpl
@@ -1,1 +1,52 @@
-<h3>System Information Test</h3>
+<p class="pageoverflow">
+{si_lang a=help_systeminformation}
+</p><hr/>
+
+<fieldset>
+<legend><strong>{si_lang a=cms_install_information}</strong>: </legend>
+<div class="pageoverflow">
+  <p class="pagetext">{si_lang a=cms_version}</p>
+  <p class="pageinput">{$cms_version}</p>
+</div>
+<h4>{si_lang a=installed_modules}</h4>
+{foreach from=$installed_modules item='module'}
+  <div class="pageoverflow">
+    <p class="pagetext">{$module.module_name}</p>
+    <p class="pageinput">{$module.version}</p>
+  </div>
+{/foreach}
+</fieldset>
+
+<fieldset>
+<legend><strong>{si_lang a=php_information}</strong>: </legend>
+{foreach from=$php_information key='key' item='one'}
+  {if is_array($one)}
+    {if empty($one[1])}
+    <div class="pageoverflow">
+      <p class="pagetext">{si_lang a=$key} ({$key}):</p>
+      <p class="pageinput">{$one[0]}</p>
+    </div>
+    {else}
+    <div class="pageoverflow" style="color: {$one[1]};">
+      <p class="pagetext">{$key}: {si_lang a=$key}</p>
+      <p class="pageinput">{$one[0]}</p>
+    </div>
+    {/if}
+  {else}
+  <div class="pageoverflow">
+    <p class="pagetext">{si_lang a=$key} ({$key}):</p>
+    <p class="pageinput">{$one}</p>
+  </div>
+  {/if}
+{/foreach}
+</fieldset>
+
+<fieldset>
+<legend><strong>{si_lang a=server_information}</strong>: </legend>
+{foreach from=$server_info key='key' item='one'}
+  <div class="pageoverflow">
+    <p class="pagetext">{si_lang a=$key}:</p>
+    <p class="pageinput">{$one}</p>
+  </div>
+{/foreach}
+</fieldset>
