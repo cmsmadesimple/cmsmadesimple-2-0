f9180a53291a5396d367ada2a59ca2bdb349a15a
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2008-07-29 04:58:59 +0000

    Fixes problem with wysiwyg being available improperly when adding a gcb.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.2.x@4898 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/addhtmlblob.php
+++ admin/addhtmlblob.php
@@ -51,7 +51,11 @@ if (get_preference($userid, 'use_wysiwyg') == "1") {
     $use_javasyntax = true;
 }
 */
-$gcb_wysiwyg = get_preference($userid, 'gcb_wysiwyg', 1);
+$gcb_wysiwyg = (get_site_preference('nogcbwysiwyg','0') == '0') ? 1 : 0;
+if( $gcb_wysiwyg )
+  {
+    $gcb_wysiwyg = get_preference($userid, 'gcb_wysiwyg', 1);
+  }
 
 
 if ($access) {
