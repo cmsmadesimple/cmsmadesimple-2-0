9f81ea1515270a8c15b673e156abf4f75fbd3a31
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2009-03-05 03:34:55 +0000

    v1.6-beta1 -- New Caledonia
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.6.x@5471 3d254a34-79dc-0310-9e5f-be208747d8a0

--- version.php
+++ version.php
@@ -18,8 +18,8 @@
 #
 #$Id$
 
-$CMS_VERSION = "1.5.3";
-$CMS_VERSION_NAME = "Arecibo";
+$CMS_VERSION = "1.6-beta1";
+$CMS_VERSION_NAME = "New Caledinia";
 $CMS_SCHEMA_VERSION = "31";
 
 define('CMS_VERSION', $CMS_VERSION);
