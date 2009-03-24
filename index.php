9ca6975f16bd38f537d0d11f1d431763d1465280
ronnyk <ronnyk@3d254a34-79dc-0310-9e5f-be208747d8a0>
2009-03-24 21:06:27 +0000

    Fixed small typo with debug set to true, peak-memory didnt end with a )
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.6.x@5499 3d254a34-79dc-0310-9e5f-be208747d8a0

--- index.php
+++ index.php
@@ -340,7 +340,7 @@ $memory = $memory - $orig_memory;
 $memory_peak = (function_exists('memory_get_peak_usage')?memory_get_peak_usage():0);
 if ( !is_sitedown() && $config["debug"] == true)
 {
-  echo "<p>Generated in ".microtime_diff($starttime,$endtime)." seconds by CMS Made Simple using ".(isset($db->query_count)?$db->query_count:'')." SQL queries and {$memory} bytes of memory (peak memory usage was {$memory_peak}</p>";
+  echo "<p>Generated in ".microtime_diff($starttime,$endtime)." seconds by CMS Made Simple using ".(isset($db->query_count)?$db->query_count:'')." SQL queries and {$memory} bytes of memory (peak memory usage was {$memory_peak})</p>";
 }
 else if( !isset($config['hide_performance_info']) )
 {
