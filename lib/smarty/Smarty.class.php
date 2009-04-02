f9da068665974db263c4c5d3cf3f56f1c47a4066
alby <alby@3d254a34-79dc-0310-9e5f-be208747d8a0>
2009-04-02 21:57:42 +0000

    Smarty 2.6.23-dev, move markdown in plugins folder
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.6.x@5522 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/smarty/Smarty.class.php
+++ lib/smarty/Smarty.class.php
@@ -27,7 +27,7 @@
  * @author Monte Ohrt <monte at ohrt dot com>
  * @author Andrei Zmievski <andrei@php.net>
  * @package Smarty
- * @version 2.6.22
+ * @version 2.6.23-dev
  */
 
 /* $Id$ */
@@ -464,7 +464,7 @@ class Smarty
      *
      * @var string
      */
-    var $_version              = '2.6.22';
+    var $_version              = '2.6.23-dev';
 
     /**
      * current template inclusion depth
