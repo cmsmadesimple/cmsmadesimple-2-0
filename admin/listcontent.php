620253aadb46a7ffe76faaf39bec745fdf8ba435
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2009-05-02 23:56:05 +0000

    Fix idiot error.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.6.x@5643 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/listcontent.php
+++ admin/listcontent.php
@@ -888,7 +888,7 @@ function display_hierarchy(&$root, &$userid, $modifyall, &$templates, &$users, &
 	  if ($one->DefaultContent() != true)
 	    {
 	      if ($root->getChildrenCount() == 0 && 
-		  (check_permission($userid, 'Remove Pages') || check_permission('Manage All Content')) )
+		  (check_permission($userid, 'Remove Pages') || check_permission($userid,'Manage All Content')) )
 		{
 		  $txt .= "<a href=\"{$thisurl}&amp;deletecontent=".$one->Id()."\" onclick=\"if (confirm('".cms_html_entity_decode_utf8(lang('deleteconfirm', $one->mName), true)."')) xajax_content_delete(".$one->Id()."); return false;\">";
 		  $txt .= $deleteImg;
