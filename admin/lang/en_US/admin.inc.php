af6bdcefeac6aed4a86d9c81d42fc05d3885cb18
ronnyk <ronnyk@3d254a34-79dc-0310-9e5f-be208747d8a0>
2009-05-02 12:37:20 +0000

    removed "content content", so the doubling.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.6.x@5627 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/lang/en_US/admin.inc.php
+++ admin/lang/en_US/admin.inc.php
@@ -873,7 +873,7 @@ $lang['admin']['help_function_content'] = <<<EOT
 		<li><em>(optional)</em>wysiwyg (true/false) - If set to false, then a wysiwyg will never be used while editing this block.  If true, then it acts as normal.  Only works when block parameter is used.</li>
 		<li><em>(optional)</em>oneline (true/false) - If set to true, then only one edit line will be shown while editing this block.  If false, then it acts as normal.  Only works when block parameter is used.</li>
 <li><em>(optional)</em>size - Applicable only when the oneline option is used this optional parameter allows you to specify the size of the edit field.  The default value is 50.</li>
-		<li><em>(optional)</em>default - Allows you to specify default content content for this content blocks (additional content blocks only).</li>
+		<li><em>(optional)</em>default - Allows you to specify default content for this content blocks (additional content blocks only).</li>
 		<li><em>(optional)</em>assign - Assigns the content to a smarty parameter, which you can then use in other areas of the page, or use to test whether content exists in it or not.
 <p>Example of passing page content to a User Defined Tag as a parameter:</p>
 <pre>
