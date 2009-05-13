74cf442ad7ae27798608ad10f4c9be0becbf5fce
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2009-05-13 15:13:41 +0000

    Update the export script.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.6.x@5703 3d254a34-79dc-0310-9e5f-be208747d8a0

--- scripts/export_sql.sh
+++ scripts/export_sql.sh
@@ -2,6 +2,7 @@
 
 _dbprefix=cms_
 _this=`basename $0`
+_excludes="adminlog additional_htmlblob_users"
 
 if [ $# -le 0 ]; then
   echo "Usage: ${_this} dbname [username] [password] [dbprefix]"
@@ -29,10 +30,25 @@ _tables=''
 for _t in $_tmp ; do
   _x=`echo $_t | grep -ce '_seq$'`
   _x1=`echo $_t | grep -ce "^${_dbprefix}"`
-  if [ $_x = 0 -a $_x1 = 1 ]; then
+
+  _x2=0
+  for _e in $_excludes ; do
+    if [ "${_dbprefix}${_e}" = $_t ]; then
+     _x2=1
+    fi
+  done
+
+  if [ $_x2 = 0 -a $_x = 0 -a $_x1 = 1 ]; then
     _tables="$_t $_tables"
   fi
 done
 
 # Dump all the tables
-mysqldump ${_opts} --skip-quote-names --compact --no-create-info ${_dbname} ${_tables} | sed -e "s/^INSERT INTO ${_dbprefix}/INSERT INTO {DB_PREFIX}/g" 
+_tmp=1
+mysqldump ${_opts} --compatible=mysql40 --skip-opt --skip-quote-names --compact --no-create-info ${_dbname} ${_tables} | sed -e "s/^INSERT INTO ${_dbprefix}/INSERT INTO {DB_PREFIX}/g" > /tmp/$_this.$$.$_tmp
+
+# post processing
+cat /tmp/$_this.$$.$_tmp | grep -v "siteprefs VALUES ('sitename"
+
+# cleanup
+rm /tmp/$_this.$$.$_tmp
