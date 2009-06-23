fb92b5937ccc37dac0afae76d10d6a9ced488f29
nuno <nuno@3d254a34-79dc-0310-9e5f-be208747d8a0>
2009-06-23 16:15:36 +0000

    added "forgot password " stuff in the default template
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.6.x@5790 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/themes/default/login.php
+++ admin/themes/default/login.php
@@ -6,7 +6,7 @@
 <head>
 <title>CMS Login</title>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo get_encoding() ?>" />
-<script src="themes/default/includes/standard.js" type="text/javascript"></script>
+<script src="themes/default/includes/standard.js" type="text/javascript"></script> 
 
 <link rel="stylesheet" type="text/css" media="screen, projection" href="loginstyle.php" />
 <base href="<?php global $gCms; $config =& $gCms->GetConfig(); echo $config['root_url'] . '/' . $config['admin_dir'] . '/'; ?>" />
@@ -24,10 +24,51 @@
 						{
 							echo '<span class="loginerror">'.$error.'</span>';
 						}
+						
+					    else if (isset($warningLogin) && $warningLogin != '')
+						{
+							echo '<span class="warningLogin">'.$warningLogin.'</span>';
+						}
+						else if (isset($acceptLogin) && $acceptLogin != '')
+						{
+							echo '<span class="acceptLogin">'.$acceptLogin.'</span>';
+						}
 					?>
 				</p>
 			</div>
+			
 			<div class="lbfields">
+			<?php if ($changepwhash != '') {
+							echo '<div class="warningLogin">'.lang('passwordchange').'</div>';
+					?>
+						<div class="lbfieldstext">
+							<p class="lbpass"><?php echo lang('password')?>:</p>
+							<p class="lbpass"><?php echo lang('passwordagain')?>:</p>
+						</div>
+						<div class="login-fields">
+							<form method="post" action="login.php">
+								<p class="lbpass"><input id="lbpassword"  name="password" type="password" size="15" /></p>
+								<p class="lbpass"><input id="lbpasswordagain"  name="passwordagain" type="password" size="15" /></p>
+								<input type="hidden" name="changepwhash" value="<?php echo $changepwhash ?>" />
+								<input type="hidden" name="forgotpwchangeform" value="1" />
+								<input class="lbsubmit" name="loginsubmit" type="submit" value="<?php echo lang('submit')?>" /> 
+								<input class="lbsubmit" name="logincancel" type="submit" value="<?php echo lang('cancel')?>" />
+							</form>
+						</div>
+					<?php } else if (isset($_REQUEST['forgotpw']) && $_REQUEST['forgotpw']) { ?>
+						<p class="lbfieldstart"><?php echo lang('forgotpwprompt')?></p>
+						<div class="lbfieldstext">
+							<p class="lbuser"><?php echo lang('username')?>:</p>
+						</div>
+						<div class="login-fields">
+							<form method="post" action="login.php">
+								<input id="lbusername" name="forgottenusername" <?php if(!isset($_POST['username'])) echo 'class="defaultfocus"' ?> type="text" size="15" value="" /><br />
+								<input type="hidden" name="forgotpwform" value="1" />
+								<input class="lbsubmit" name="loginsubmit" type="submit" value="<?php echo lang('submit')?>" /> 
+								<input class="lbsubmit" name="logincancel" type="submit" value="<?php echo lang('cancel')?>" />
+							</form>
+						</div>
+					<?php } else { ?>
 				<div class="lbfieldstext">
 					<p class="lbuser"><?php echo lang('username')?>:</p>
 					<p class="lbpass"><?php echo lang('password')?>:</p>
@@ -45,8 +86,12 @@
 							<input class="lbsubmit" name="logincancel" type="submit" value="<?php echo lang('cancel')?>" />
 						</p>
 					</form>
+						<div class="forgot-pw">
+						<a href="login.php?forgotpw=1"><?php echo lang('lostpw')?></a>
+					</div>
 				</div>
-			</div>
+				<?php } ?>
+			</div>	
 	</div>
 	<div class="lblayout footer"><a class="footer" href="http://www.cmsmadesimple.org">CMS Made Simple</a> is free software released under the General Public Licence.</div>
 </div>	
