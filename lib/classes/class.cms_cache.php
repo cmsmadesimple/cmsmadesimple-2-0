cdf80a22de40b249df1919904fd5705c2ad39e88
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2009-08-12 01:04:58 +0000

    Add CmsCache class
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/1.7.x@5887 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ lib/classes/class.cms_cache.php
@@ -0,0 +1,112 @@
+<?php // -*- mode:php; tab-width:4; indent-tabs-mode:t; c-basic-offset:4; -*-
+#CMS - CMS Made Simple
+#(c)2004-2008 by Ted Kulp (ted@cmsmadesimple.org)
+#This project's homepage is: http://cmsmadesimple.org
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#$Id$
+
+/**
+ * Wrapper class around Cache:Lite for caching full pages and function output.
+ *
+ * @author Ted Kulp
+ * @since 2.0
+ * @version $Revision$
+ * @modifiedby $LastChangedBy$
+ * @lastmodified $Date$
+ * @license GPL
+ **/
+class CmsCache extends CmsObject
+{
+	static private $instances = null;
+	private $cache = null;
+	
+	function __construct($type = 'function')
+	{
+		parent::__construct();
+
+		// Set a few options
+		$options = array(
+		    'cacheDir' => cms_join_path(ROOT_DIR, 'tmp', 'cache'.DS),
+		    'lifeTime' => 300
+		);
+
+		if ($type == 'function')
+		{
+			//if (!CmsConfig::get('function_caching') || CmsConfig::get('debug'))
+			if (!CmsConfig::get('function_caching'))
+				$options['caching'] = false;
+
+			require_once(cms_join_path(ROOT_DIR, 'lib', 'pear', 'cache', 'lite', 'Function.php'));
+			$this->cache = new Cache_Lite_Function($options);
+		}
+		else
+		{
+			require_once(cms_join_path(ROOT_DIR, 'lib', 'pear', 'cache', 'lite', 'Function.php'));
+			$this->cache = new Cache_Lite($options);
+		}
+	}
+	
+	public static function get_instance($type = 'function')
+	{
+		if (self::$instances == null)
+		{
+			self::$instances = array();
+		}
+
+		if (empty(self::$instances[$type]))
+		{
+			self::$instances[$type] = new CmsCache($type);
+		}
+
+		return self::$instances[$type];
+	}
+	
+	public function get($id, $group = 'default', $doNotTestCacheValidity = FALSE)
+	{
+		return $this->cache->get($id, $group, $doNotTestCacheValidity);
+	}
+	
+	public function save($data, $id = NULL, $group = 'default')
+	{
+		return $this->cache->save($data, $id, $group);
+	}
+	
+	public function call()
+	{
+		$args = func_get_args();
+		return call_user_func_array(array($this->cache, 'call'), $args);
+	}
+	
+	public function drop()
+	{
+		$args = func_get_args();
+		return call_user_func_array(array($this->cache, 'drop'), $args);
+	}
+	
+	public function clean($group = FALSE, $mode = 'ingroup')
+	{
+		CmsContentOperations::clear_cache();
+		return $this->cache->clean($group, $mode);
+	}
+	
+	static public function clear($group = FALSE, $mode = 'ingroup')
+	{
+		return self::get_instance()->clean($group, $mode);
+	}
+}
+
+# vim:ts=4 sw=4 noet
+?>
